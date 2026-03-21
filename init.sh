a2a02055bc1f0dfa29de3f7e58a6c8a359e6eaa2
stuiterveer <admin@stuiterveer.com>
2026-03-21 23:36:24 +0100

    Create QML app that loads before ever running freej2me

--- init.sh
+++ init.sh
@@ -4,7 +4,18 @@ if ! [ -z ${APP_ID} ]; then
         appName=$(echo ${APP_ID} | utils/cut.py '_' 1)
         activeDir=${XDG_CONFIG_HOME}/${appName}
         utils/mkdir.py ${activeDir}
-        cd ${activeDir}
     fi
 fi
-${clickDir}/runtime/bin/java -jar ${clickDir}/freej2me.jar "file://${clickDir}/midlet.jar"
+
+DOLOOP=true
+while ${DOLOOP}; do
+    DOLOOP=false
+    cd ${clickDir}
+    qmlscene qml/Main.qml
+    if [ -f ${activeDir}/runJar ]; then
+        DOLOOP=true
+        rm ${activeDir}/runJar
+        cd ${activeDir}
+        ${clickDir}/runtime/bin/java -jar ${clickDir}/freej2me.jar "file://${clickDir}/midlet.jar"
+    fi
+done
---
+++ qml/Main.qml
@@ -0,0 +1,22 @@
+import QtQuick 2.7
+import Lomiri.Components 1.3
+import Qt.labs.settings 1.0
+
+MainView {
+    id: root
+    objectName: 'mainView'
+    applicationName: 'jarman.stuiterveer'
+    automaticOrientation: true
+
+    width: units.gu(45)
+    height: units.gu(75)
+
+    Page {
+        anchors.fill: parent
+
+        header: PageHeader {
+            id: header
+            title: 'JarMan'
+        }
+    }
+}
